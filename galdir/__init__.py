f0101a06214e6d038971d2e4b0b2157c6909683e
Sam Burney <sam@burney.io>
2019-04-14 17:00:24 +0930

    Very rudimentary gallery implementation

--- galdir/__init__.py
+++ galdir/__init__.py
@@ -1,9 +1,17 @@
+import os
 from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
+
     config.add_route('home', '/')
-    config.add_route('hello', '/hello')
+    config.add_route('view', '/view/{path:.*}')
+
+    dir_script = os.path.dirname(__file__)
+    dir_app = os.path.abspath(os.path.join(dir_script, '..'))
+    dir_albums = os.path.normpath(os.path.join(dir_app, settings['galdir.dir_albums']))
+    config.add_static_view('albums', dir_albums)
+
     config.scan('.views')
     return config.make_wsgi_app()
--- galdir/views.py
+++ galdir/views.py
@@ -1,3 +1,5 @@
+import os
+import re
 from pyramid.response import Response
 from pyramid.view import view_config
 
@@ -7,9 +9,62 @@ from pyramid.view import view_config
     renderer='templates/home.jinja2'
 )
 def home(request):
-    return {}
+    settings = request.registry.settings
+    
+    dir_script = os.path.dirname(__file__)
+    dir_app = os.path.abspath(os.path.join(dir_script, '..'))
+    dir_albums = os.path.normpath(os.path.join(dir_app, settings['galdir.dir_albums']))
 
+    if(os.path.isdir(dir_albums)):
+        contents = os.listdir(dir_albums)
 
-@view_config(route_name='hello')
-def hello(request):
-    return Response('<body>Go back <a href="/">home</a></body>')
+        # Process resulting directory list
+        items = []
+        for content in contents:
+            if re.search(r'^\.', content) == None:
+                if os.path.isdir(os.path.join(dir_albums, content)):
+                    type = 'dir'
+                else:
+                    type = 'file'
+
+                items.append({
+                    'name': content,
+                    'path': content,
+                    'type': type,
+                })
+
+    return {'items': items}
+
+@view_config(
+    route_name = 'view',
+    renderer='templates/home.jinja2'
+)
+def view(request):
+    settings = request.registry.settings
+
+    dir_script = os.path.dirname(__file__)
+    dir_app = os.path.abspath(os.path.join(dir_script, '..'))
+    dir_albums = os.path.normpath(os.path.join(
+        dir_app, settings['galdir.dir_albums']))
+    dir_view = os.path.normpath(os.path.join(
+        dir_app, dir_albums, request.matchdict['path']))
+
+    if os.path.isdir(dir_view):
+        contents = os.listdir(dir_view)
+
+        # Process resulting directory list
+        items = []
+        for content in contents:
+            if re.search(r'^\.', content) == None:
+                if os.path.isdir(os.path.join(dir_view, content)):
+                    type = 'dir'
+                else:
+                    type = 'file'
+
+                items.append({
+                    'name': content,
+                    'path': request.matchdict['path'] + '/' + content,
+                    'type': type,
+                })
+
+    return {'items': items}
